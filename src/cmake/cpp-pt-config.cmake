da86ad8e2e9f592a9377146c5aa8a810d625ed6a
Mads Doest <doest@greenwood.dk>
2025-05-05 09:42:22 +0200

    removed unwanted deps

--- src/cmake/cpp-pt-config.cmake
+++ src/cmake/cpp-pt-config.cmake
@@ -3,8 +3,6 @@
 
 include(CMakeFindDependencyMacro)
 
-find_dependency(fmt)
 find_dependency(Microsoft.GSL)
-find_dependency(range-v3)
 
 include("${CMAKE_CURRENT_LIST_DIR}/@cpp_pt_name@-targets.cmake")
